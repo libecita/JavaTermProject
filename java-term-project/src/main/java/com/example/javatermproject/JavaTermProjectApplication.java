798058f2909e4cfb7f88c63a99670b58bc64fce1
libecita <167230409+libecita@users.noreply.github.com>
2024-11-05 14:58:45 -0500

    Add files via upload

---
+++ java-term-project/src/main/java/com/example/javatermproject/JavaTermProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.javatermproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTermProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTermProjectApplication.class, args);
+    }
+
+}
---
+++ java-term-project/src/test/java/com/example/javatermproject/JavaTermProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javatermproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTermProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
