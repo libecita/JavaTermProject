027b68c9a01747e3a7a119977df58d31ca52f227
Tanya Clarisse <2330949@champlaincollege.qc.ca>
2024-11-07 16:50:58 -0500

    Added schema.sql, added the "implements" to the ServiceImpl. Started a bit with LikeController

--- java-term-project/src/main/java/com/example/javatermproject/businessLayer/LikeServiceImpl.java
+++ java-term-project/src/main/java/com/example/javatermproject/businessLayer/LikeServiceImpl.java
@@ -1,9 +1,9 @@
 package com.example.javatermproject.businessLayer;
 
-import com.example.javatermproject.presentationLayer.LikeResponseModel;
+//import com.example.javatermproject.presentationLayer.LikeResponseModel;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+//import java.util.List;
 
 @Service
 public class LikeServiceImpl implements LikeService {
--- java-term-project/src/main/java/com/example/javatermproject/presentationLayer/LikeController.java
+++ java-term-project/src/main/java/com/example/javatermproject/presentationLayer/LikeController.java
@@ -1,12 +1,12 @@
 package com.example.javatermproject.presentationLayer;
 
-import com.example.javatermproject.businessLayer.LikeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+//import com.example.javatermproject.businessLayer.LikeService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+//import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/likes")
