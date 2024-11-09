df576b81facc46573417d5a05b49fb3ad5f5ec24
libecita <mancebolizbeth22@hotmail.com>
2024-11-09 18:32:49 -0500

    Worked on PostController.

--- java-term-project/src/main/java/com/example/javatermproject/presentationLayer/LikeController.java
+++ java-term-project/src/main/java/com/example/javatermproject/presentationLayer/LikeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/likes")
+@RequestMapping("/api/v1/likes")
 public class LikeController {
 
     private LikeService likeService;
--- java-term-project/src/main/java/com/example/javatermproject/presentationLayer/PostController.java
+++ java-term-project/src/main/java/com/example/javatermproject/presentationLayer/PostController.java
@@ -1,4 +1,41 @@
 package com.example.javatermproject.presentationLayer;
 
+@RestController()
+@RequestMapping("/api/v1/posts")
 public class PostController {
+
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping()
+    public List<PostResponseModel> getPosts() {
+        return postService.getPosts();
+    }
+
+    @GetMapping("/{post_id}")
+    public PostResponseModel getPostsById(@PathVariable String post_id) {
+        return postService.getPostsById(post_id);
+    }
+
+    @PostMapping("/{post_id}")
+    public PostResponseModel addOnePost(@RequestBody PostRequestModel postRequestModel) {
+        return postService.addOnePost(postRequestModel);
+    }
+
+    @PutMapping("/{post_id}")
+    public PostResponseModel updatePost(@PathVariable String post_id
+                                        @RequestBody PostRequestModel postRequestModel) {
+        return postService.updatePost(post_id, postRequestModel);
+    }
+
+    @DeleteMapping("/{post_id}")
+    public String deletePost(@PathVariable String post_id) {
+        return postService.deletePost(post_id);
+    }
+
+
 }
