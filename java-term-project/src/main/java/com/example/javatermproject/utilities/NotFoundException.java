e585635dad3e1ddad6bf10f5746896880bba13ee
libecita <mancebolizbeth22@hotmail.com>
2024-11-11 14:45:21 -0500

    Added NotFoundException.java in utilities folder.

---
+++ java-term-project/src/main/java/com/example/javatermproject/utilities/NotFoundException.java
@@ -0,0 +1,9 @@
+package org.example.movielistapp.utilities;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException() {}
+    public NotFoundException(String message) { super(message); }
+    public NotFoundException(Throwable cause) { super(cause); }
+    public NotFoundException(String message, Throwable cause) { super(message, cause); }
+
+}
