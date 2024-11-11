fbc170ca708d09120f858648e3abced9cb384c77
libecita <mancebolizbeth22@hotmail.com>
2024-11-11 17:59:04 -0500

    Updated PostRequestModel.java, although I'm not super sure about it. Feel free to make changes.

--- java-term-project/src/main/java/com/example/javatermproject/presentationLayer/PostRequestModel.java
+++ java-term-project/src/main/java/com/example/javatermproject/presentationLayer/PostRequestModel.java
@@ -8,4 +8,13 @@ import org.springframework.stereotype.Component;
 @Data
 @Component
 public class PostRequestModel {
+
+    private String postIdentifier;
+
+    private String datePosted;
+
+    private String description;
+
+    private String title;
+
 }
